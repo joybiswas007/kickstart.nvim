d7985b66377afda76a9a0ad0e433e860c01bc313
Joy Biswas <joybiswas040701@gmail.com>
2026-03-21 13:37:39 +0600

    fix: indentation width for templ files

--- init.lua
+++ init.lua
@@ -155,6 +155,16 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   callback = function() vim.hl.on_yank() end,
 })
 
+-- Fix indentation width for templ files
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = 'templ',
+  callback = function()
+    vim.opt_local.tabstop = 2
+    vim.opt_local.shiftwidth = 2
+    vim.opt_local.expandtab = true
+  end,
+})
+
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
@@ -750,6 +760,7 @@ require('lazy').setup({
     config = function()
       ---@diagnostic disable-next-line: missing-fields
       require('gruvbox').setup {
+        -- transparent_mode = true,
         styles = {
           comments = { italic = false }, -- Disable italics in comments
         },
